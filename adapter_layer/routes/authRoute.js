a08d31f0d7bc444dfecbcdce80b9256edc26ee01
FRONTEND-CASANOVA <casanovaharry11@gmail.com>
2026-05-28 11:28:45 +0800

    student portal

--- adapter_layer/routes/authRoute.js
+++ adapter_layer/routes/authRoute.js
@@ -4,6 +4,7 @@ import express from "express";
 const authRoutes = express.Router();
 
 authRoutes.post('/register', AuthController.registerStudent);
+authRoutes.get('/students', AuthController.getStudentsInfo);
 
 
 export default authRoutes;
